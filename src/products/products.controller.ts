e5f4271d7aea0b721659f450829c5131bd65a11b
AishuPola <aishwarya.pola@proclink.com>
2024-10-25 15:50:49 +0530

    update product details is done

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-pro.dto';
+import { UpdateProductDto } from './dto/update-pro.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,5 +9,16 @@ export class ProductsController {
   @Post()
   createProduct(@Body() CreateProductDto: CreateProductDto) {
     return this.productsservice.createProduct(CreateProductDto);
+  }
+  @Get()
+  getProducts() {
+    return this.productsservice.getProducts();
+  }
+  @Put(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body() UpdateProductDto: UpdateProductDto,
+  ) {
+    return this.productsservice.updateProduct(id, UpdateProductDto);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,17 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Product } from './schema/product.schema';
 import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-pro.dto';
+import { UpdateProductDto } from './dto/update-pro.dto';
 
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectModel(Product.name) private ProductModel: Model<Product>,
   ) {}
+
   createProduct(CreateProductDto: CreateProductDto): Promise<Product> {
     const product = new this.ProductModel(CreateProductDto).save();
     console.log(product);
     return product;
+  }
+  getProducts(): Promise<Product[]> {
+    return this.ProductModel.find().exec();
+  }
+  async updateProduct(
+    id: string,
+    UpdateProductDto: UpdateProductDto,
+  ): Promise<Product> {
+    const updatedProduct = await this.ProductModel.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      UpdateProductDto,
+      { new: true },
+    ).exec();
+    if (!updatedProduct) {
+      throw new NotFoundException('product not found');
+    }
+    return updatedProduct;
   }
 }
