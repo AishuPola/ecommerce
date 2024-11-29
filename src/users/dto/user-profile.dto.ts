8dbe2e343fcbea0f4d2e973c5f45502866d9b10d
AishuPola <aishwarya.pola@proclink.com>
2024-11-29 15:29:05 +0530

    added validation to user profile

--- src/users/dto/user-profile.dto.ts
+++ src/users/dto/user-profile.dto.ts
@@ -1,12 +1,19 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserProfileDto {
   id: string;
   fullName: string;
   username: string;
+  @IsEmail()
   email: string;
   phoneNumber: string;
+  @IsString()
   country: string;
+  @IsString()
   address?: string;
+  @IsString()
   profilePicture?: string;
+
   isVerified: boolean;
   role: string;
 }
