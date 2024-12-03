7f74b22077e5ad1a3055f0c49fea0df478b71c91
AishuPola <aishwarya.pola@proclink.com>
2024-12-03 11:35:43 +0530

    enabled cors for connecting with F.E

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ async function bootstrap() {
   //const app = await NestFactory.create(AppModule);
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 4000);
 }
